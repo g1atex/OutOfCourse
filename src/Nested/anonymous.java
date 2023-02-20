a9127f7339ce00ffc0452d1591d6ea07806a510f
g1atex <cihatduger@gmail.com>
2023-02-20 15:33:48 +0300

    deneme 1

---
+++ src/Nested/anonymous.java
@@ -0,0 +1,30 @@
+package Nested;
+
+interface Faktoriyel{
+    double hesap();
+}
+
+public class anonymous {
+        public Faktoriyel fak(int s){//kitapta anonymous class lar sadece final tipteki variable lara eirşir yazıyor ama bu yanlış final olmasada erişti.
+            return new Faktoriyel() { //anonymous class return edilebildi bir değer gibi işlem gördü.
+                public double hesap(){
+                    int x=1;
+                    for(int i=s; i>0; i-- ){
+                        x=x*i;
+                    }
+                    return x;
+                }
+
+            };
+        }
+
+    public static void main(String[] args) {
+        anonymous a=new anonymous();
+        Faktoriyel f=a.fak(4); //bu şekildede yazılabilir normalde direk new deyip anonymous class oluşturuyorduk ama buda farklı bir bakış.
+        System.out.println(f.hesap()); //functioal interfacedeki abstract metodu temsil etti
+        System.out.println("git deneme");
+        System.out.println("git deneme 2" );
+        System.out.println("git deneme 3");
+    }
+
+}
